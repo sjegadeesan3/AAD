3533dd2fcaf8230a5bfe5a4d1255a755c11fd550
JEGADEESAN <sjegadeesan3@gmail.com>
2021-03-09 00:14:00 +0800

    Display snackbar

--- app/src/main/java/com/jegadeesan/ui/activity/SnackBarActivity.kt
+++ app/src/main/java/com/jegadeesan/ui/activity/SnackBarActivity.kt
@@ -2,8 +2,12 @@ package com.jegadeesan.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import com.jegadeesan.R
 import com.jegadeesan.databinding.ActivitySnackBarBinding
+import org.koin.android.ext.android.bind
 
 class SnackBarActivity : AppCompatActivity() {
 
@@ -18,6 +22,7 @@ class SnackBarActivity : AppCompatActivity() {
 
     private fun initView() {
         initToolbar()
+        initButtons()
     }
 
     private fun initToolbar() {
@@ -29,6 +34,24 @@ class SnackBarActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+
+    private fun initButtons() {
+        binding.showSnackBar.setOnClickListener {
+            Snackbar.make(binding.root, getString(R.string.snackbar_text), Snackbar.LENGTH_SHORT).show()
+        }
+        binding.showSnackbarCenter.setOnClickListener {
+            Snackbar.make(binding.snackbarCoordinatorLayout, getString(R.string.snackbar_text), Snackbar.LENGTH_SHORT).show()
+        }
+        binding.showSnackbarWithAction.setOnClickListener {
+            val snackbar = Snackbar.make(binding.root, getString(R.string.snackbar_text), Snackbar.LENGTH_SHORT)
+            snackbar.setAction(R.string.close) {
+                snackbar.dismiss()
+            }
+            snackbar.show()
+
+        }
     }
 
 }
