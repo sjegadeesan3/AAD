0bc5c02cb676e5c230ee0fb5db8f424efb6c4435
JEGADEESAN <sjegadeesan3@gmail.com>
2021-03-08 23:26:42 +0800

    Display custom toast

--- app/src/main/java/com/jegadeesan/ui/activity/ToastActivity.kt
+++ app/src/main/java/com/jegadeesan/ui/activity/ToastActivity.kt
@@ -2,9 +2,11 @@ package com.jegadeesan.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Toast
 import com.jegadeesan.R
 import com.jegadeesan.databinding.ActivityToastBinding
+import com.jegadeesan.databinding.CustomToastLayoutBinding
 
 class ToastActivity : AppCompatActivity() {
 
@@ -39,6 +41,17 @@ class ToastActivity : AppCompatActivity() {
         }
         binding.longToast.setOnClickListener {
             Toast.makeText(this, getString(R.string.long_toast_message), Toast.LENGTH_LONG).show()
+        }
+
+        binding.customToast.setOnClickListener {
+            val customToastLayoutBinding = CustomToastLayoutBinding.inflate(layoutInflater)
+            customToastLayoutBinding.toastText.text = getString(R.string.custom_toast)
+            with(Toast(applicationContext)) {
+                setGravity(Gravity.CENTER_VERTICAL, 0, 0)
+                duration = Toast.LENGTH_SHORT
+                view =customToastLayoutBinding.root
+                show()
+            }
         }
     }
 }
